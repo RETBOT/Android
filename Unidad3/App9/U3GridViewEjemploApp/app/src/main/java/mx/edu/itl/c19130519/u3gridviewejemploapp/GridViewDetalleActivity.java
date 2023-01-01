55e06f99798808649a36951dde03fa62b3176033
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 18:51:24 -0600

    Update GridViewDetalleActivity.java

--- Unidad3/App9/U3GridViewEjemploApp/app/src/main/java/mx/edu/itl/c19130519/u3gridviewejemploapp/GridViewDetalleActivity.java
+++ Unidad3/App9/U3GridViewEjemploApp/app/src/main/java/mx/edu/itl/c19130519/u3gridviewejemploapp/GridViewDetalleActivity.java
@@ -1,5 +1,5 @@
 package mx.edu.itl.c19130519.u3gridviewejemploapp;
-
+//  By: RETBOT
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,7 +8,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 public class GridViewDetalleActivity extends AppCompatActivity {
-
+//  By: RETBOT
     private ImageView imgvEscudoDetalle;
 
     @Override
@@ -22,4 +22,4 @@ public class GridViewDetalleActivity extends AppCompatActivity {
         imgvEscudoDetalle.setScaleType(ImageView.ScaleType.FIT_XY);
         Glide.with(imgvEscudoDetalle.getContext()).load(escudo).into(imgvEscudoDetalle);
     }
-}
+}//  By: RETBOT
