1255cd9b9212eaf3a1aa776c43205e25e861c2d0
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 18:52:29 -0600

    Update Club.java

--- Unidad3/App8/U3SpinnerApp/app/src/main/java/modelo/Club.java
+++ Unidad3/App8/U3SpinnerApp/app/src/main/java/modelo/Club.java
@@ -1,6 +1,6 @@
 package modelo;
-
-public class Club {
+//  By: RETBOT
+public class Club {//  By: RETBOT
     private String nombre;
     private int    escudoID;
 
@@ -11,7 +11,7 @@ public class Club {
 
     public String getNombre() {
         return nombre;
-    }
+    }//  By: RETBOT
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -25,3 +25,4 @@ public class Club {
         this.escudoID = escudoID;
     }
 }
+//  By: RETBOT
