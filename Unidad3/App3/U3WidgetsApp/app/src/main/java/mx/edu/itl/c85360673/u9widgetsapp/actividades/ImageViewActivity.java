82dcad582ddfb0c15136357a8b1d81bbe36405dc
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 18:43:10 -0600

    Update ImageViewActivity.java

--- Unidad3/App3/U3WidgetsApp/app/src/main/java/mx/edu/itl/c85360673/u9widgetsapp/actividades/ImageViewActivity.java
+++ Unidad3/App3/U3WidgetsApp/app/src/main/java/mx/edu/itl/c85360673/u9widgetsapp/actividades/ImageViewActivity.java
@@ -1,14 +1,14 @@
 package mx.edu.itl.c85360673.u9widgetsapp.actividades;
-
+//  By: RETBOT
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import mx.edu.itl.c85360673.u9widgetsapp.R;
-
+//  By: RETBOT
 public class ImageViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imageview);
     }
-}
+}//  By: RETBOT
